17b1cbe681d115a6d7c7b5dc5f7fe74f722122ce
wisdommen <wisdommen@users.noreply.github.com>
2024-02-04 18:04:02 +1100

    init

---
+++ src/main/java/com/ultikits/plugins/home/PluginMain.java
@@ -0,0 +1,37 @@
+package com.ultikits.plugins.home;
+
+import com.ultikits.plugins.home.config.HomeConfig;
+import com.ultikits.plugins.home.entity.HomeEntity;
+import com.ultikits.ultitools.abstracts.UltiToolsPlugin;
+import com.ultikits.ultitools.annotations.UltiToolsModule;
+import com.ultikits.ultitools.interfaces.DataOperator;
+import com.ultikits.ultitools.interfaces.DataStore;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+
+@UltiToolsModule
+public class PluginMain extends UltiToolsPlugin {
+
+    @Getter
+    private static PluginMain pluginMain;
+
+    public PluginMain() {
+        super();
+        pluginMain = this;
+    }
+
+    @Override
+    public boolean registerSelf() {
+        return true;
+    }
+
+    @Override
+    public void unregisterSelf() {
+    }
+
+    @Bean
+    public HomeConfig homeConfig() {
+        return getConfig("config/config.yml", HomeConfig.class);
+    }
+}
